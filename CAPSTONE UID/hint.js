533807ba5e4a375309f39e14f1ea8ee598f08218
CharishmaLotti <119773857+CharishmaLotti@users.noreply.github.com>
2022-12-11 23:33:10 +0530

    Add files via upload

---
+++ CAPSTONE UID/hint.js
@@ -0,0 +1,12 @@
+//var send = document.getElementById("ht");
+//send.addEventListener("click",openhint);
+
+function openhint(){
+    const maindiv = document.getElementById("hnt");
+    let userdiv = document.createElement("div");
+    userdiv.id = "hintbox";
+    userdiv.innerHTML = ` <div class="hntbx">
+    <p> this is a hint which <br> gives you an <br> idea of how to <br> approach the problem <br>hdjskn lleudbfs sfss gd <br> xodndo dgds agac fswcb</p>
+ </div>`;
+ maindiv.appendChild(userdiv);
+}
